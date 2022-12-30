17317bda8e643cd73b807a69091a41b57fb02837
Ethan James <lskywalker416@gmail.com>
2022-12-30 12:54:54 -0700

    remove marquee cause stupid

--- js/main.js
+++ js/main.js
@@ -1,19 +1,1 @@
 console.log("Hello World!");
-
-/**
- * Example of starting a plugin with options.
- * I am just passing some of the options in the following example.
- * you can also start the plugin using $('.marquee').marquee(); with defaults
-*/
-$('.marquee').marquee({
-	//duration in milliseconds of the marquee
-	duration: 7000,
-	//gap in pixels between the tickers
-	gap: 0,
-	//time in milliseconds before the marquee will start animating
-	delayBeforeStart: 0,
-	//'left' or 'right'
-	direction: 'right',
-	//true or false - should the marquee be duplicated to show an effect of continues flow
-	duplicated: true
-});
